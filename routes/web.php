2d7c1c3e17955aa07d521d9d2ba98f490ac6714e
sakigoma <sakichan9988@gmail.com>
2024-03-13 21:53:05 +0900

    ログイン状態でのユーザーネーム表示

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use App\Http\Controllers\ArticleController;
 |
 */
 
-Route::get('/','ArticleController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
