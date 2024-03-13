fc152cb728fd1023ed9d52c625cc1b57bb0ddb9c
sakigoma <sakichan9988@gmail.com>
2024-03-13 17:43:30 +0900

    index.blade.php作成

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,20 @@
+@extends('app')
+
+@section('title', '記事一覧')
+
+@section('content')
+    <div>
+        ユーザー名
+    </div>
+    <div>
+        2020/2/1 12:00
+    </div>
+    <div>
+        <h3>
+          記事タイトル
+        </h3>
+    <div>
+        <div>
+          記事本文
+        </div>
+@endsection
