78abd3eb42d206fbd5d69ed62899ecf160aa0767
sakigoma <sakichan9988@gmail.com>
2024-03-15 11:59:21 +0900

    一覧画面ルート設定

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::get('post/create', [PostController::class, 'create']);
 
 Route::post('post', [PostController::class, 'store'])->name('post.store');
 
+Route::get('post', [PostController::class, 'index']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
