6f737b000373aa0e2e71b68ad3b6b249bcd9d7c0
sakigoma <sakichan9988@gmail.com>
2024-03-15 15:16:49 +0900

    書き込んだユーザーネーム表示

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -15,7 +15,7 @@
                 {{ $post->body }}
             <div class="p-4 text-sm font-semibold">
                 <p>
-                    {{ $post->created_at }}
+                    {{ $post->created_at }} / {{ $post->user->name }}
                 </p>
             </div>
         </div>
