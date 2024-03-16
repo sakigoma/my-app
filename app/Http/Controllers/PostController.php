32777206af4bcaecccc15eaf45f9f961f00a5ff4
sakigoma <sakichan9988@gmail.com>
2024-03-16 14:39:58 +0900

    タイプヒント

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 
     public function store(Request $request) {
         Gate::authorize('test');
-        
+
         $validated = $request->validate([
             'title' => 'required|max:20',
             'body' => 'required|max:400',
@@ -31,6 +31,10 @@ class PostController extends Controller
     public function index() {
         $posts = Post::all();
         return view('post.index', compact('posts'));
+    }
+
+    public function show(Post $post) {
+        return view('post.show', compact('post'));
     }
 }
 
