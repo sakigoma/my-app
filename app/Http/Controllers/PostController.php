b6bb052d300c2ffd1b4c1afff2f22d888923c9f4
sakigoma <sakichan9988@gmail.com>
2024-03-16 20:49:43 +0900

    編集と更新の処理

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -35,6 +36,24 @@ class PostController extends Controller
 
     public function show(Post $post) {
         return view('post.show', compact('post'));
+    }
+
+    public function edit(Post $post) {
+        return view('post.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post) {
+        $validated = $request->validate([
+            'title' => 'required|max:20',
+            'body' => 'required|max:400',
+        ]);
+
+        $validated['user_id'] = auth()->id();
+
+        $post->update($validated);
+
+        $request->session()->flash('message', '更新しました');
+        return back();
     }
 }
 
